f1b8e924dfe6264b7d91bcf09130175a658722d3
dipon.kumer <dipon.kumer@bjitgroup.com>
2023-10-30 11:45:17 +0600

    New Design is added for now graph is showing good graph will be plotted in real time

--- collatz.js
+++ collatz.js
@@ -1,38 +1,65 @@
+let plotData = {
+    x: [],
+    y: [],
+    type: 'scatter',
+    mode: 'lines+markers',
+    name: 'Collatz Sequence',
+};
+
+let layout = {
+    title: 'Collatz Sequence',
+    xaxis: { title: 'Step' },
+    yaxis: { title: 'Value' },
+};
+
+let config = {
+    responsive: true,
+};
+
+const plot = Plotly.newPlot('plot', [plotData], layout, config);
+
 function collatz(n) {
-    const sequence = [n];
-    while (n !== 1) {
-        if (n % 2 === 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
-        sequence.push(n);
+    let sequence = [n];
+
+    function updatePlot(step, value) {
+        Plotly.extendTraces('plot', { x: [[step]], y: [[value]] }, [0]);
     }
-    return sequence;
+
+    function generateNext() {
+        if (n !== 1) {
+            if (n % 2 === 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            sequence.push(n);
+            const step = sequence.length - 1;
+            updatePlot(step, n);
+            requestAnimationFrame(generateNext);
+        }
+    }
+
+    generateNext();
 }
 
-document.getElementById("plotButton").addEventListener("click", function () {
-    const n = parseInt(document.getElementById("inputNumber").value);
+document.getElementById('plotButton').addEventListener('click', () => {
+    const n = parseInt(document.getElementById('inputNumber').value);
     if (isNaN(n) || n <= 0) {
-        alert("Please enter a positive integer.");
+        alert('Please enter a positive integer.');
         return;
     }
 
-    const sequence = collatz(n);
-
-    const data = [{
-        x: Array.from({ length: sequence.length }, (_, i) => i),
-        y: sequence,
+    plotData = {
+        x: [0],
+        y: [n],
         type: 'scatter',
         mode: 'lines+markers',
         name: 'Collatz Sequence',
-    }];
-
-    const layout = {
-        title: `Collatz Sequence for ${n}`,
-        xaxis: { title: 'Step' },
-        yaxis: { title: 'Value' },
     };
 
-    Plotly.newPlot('plot', data, layout);
+    layout.title = `Collatz Sequence for ${n}`;
+
+    Plotly.react('plot', [plotData], layout, config);
+
+    collatz(n);
 });
