b9f3b283c035fdb17293ba6b33bd149e6ccfab48
dipon.kumer <dipon.kumer@bjitgroup.com>
2023-10-30 12:45:13 +0600

    real-time Collatz graph and one for the step-by-step Collatz graph added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+import time
 import plotly.io as pio
-import requests
 
 def collatz(n):
     """Generates the Collatz sequence for the given number n.
@@ -34,20 +36,55 @@ def plot_collatz(n):
     sequence = collatz(n)
 
     # Create the Plotly figure
-    fig = go.Figure()
+    fig = make_subplots(
+        rows=1, cols=2,
+        subplot_titles=('Real-time Collatz Graph', 'Step-by-step Collatz Graph')
+    )
 
-    # Add the scatter plot trace
-    fig.add_trace(go.Scatter(x=list(range(len(sequence))), y=sequence, mode='lines+markers', name='Collatz Sequence'))
+    # Add the real-time scatter plot trace
+    fig.add_trace(go.Scatter(x=list(range(len(sequence))), y=sequence, mode='lines+markers', name='Collatz Sequence'), row=1, col=1)
 
-    # Set the layout title
-    fig.update_layout(title=f'Collatz Sequence for {n}')
+    # Add the step-by-step scatter plot trace
+    fig.add_trace(go.Scatter(x=list(range(len(sequence))), y=sequence, mode='markers', name='Collatz Sequence'), row=1, col=2)
+
+    # Set the layout
+    fig.update_layout(title='Collatz Sequence', xaxis_title='Step', yaxis_title='Value')
 
     # Plot the figure
     pio.show(fig)
+
+def update_realtime_graph(fig, sequence):
+    """Updates the real-time Collatz graph.
+
+    Args:
+        fig: The Plotly figure object.
+        sequence: The Collatz sequence.
+    """
+
+    # Get the real-time scatter plot trace
+    trace = fig.data[0]
+
+    # Update the trace data
+    trace.x = list(range(len(sequence)))
+    trace.y = sequence
+
+    # Redraw the graph
+    pio.update(fig)
 
 if __name__ == '__main__':
     # Get the input number from the user
     n = int(input('Enter a positive integer: '))
 
-    # Plot the Collatz sequence for the given number
+    # Plot the Collatz sequence
     plot_collatz(n)
+
+    # Start the real-time update loop
+    while True:
+        # Generate the next value in the Collatz sequence
+        n = collatz(n)[-1]
+
+        # Update the real-time graph
+        update_realtime_graph(fig, collatz(n))
+
+        # Wait for a short period of time
+        time.sleep(0.1)
